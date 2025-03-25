ce8a5725aaf2075a948e4bb4af951c2e67746711
bagjuna <juna.park124@gmail.com>
2025-03-25 15:21:28 +0900

    day 20

---
+++ src/main1/day24.java
@@ -0,0 +1,23 @@
+package main1;
+
+import java.util.Scanner;
+
+/**
+ * 22864번 피로도
+ * dfasfdasf
+ */
+public class day24 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        int M = sc.nextInt();
+
+
+
+    }
+
+}
